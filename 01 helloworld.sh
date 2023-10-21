aeb3bfda7767e404106892c3a6a72bb8566dd752
srilakshmi <srilakshmidhulipalla34@gmail.com>
2023-10-21 19:41:20 -0400

    hello world script added

---
+++ 01 helloworld.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "hello world i am learning devops with aws"
+echo "ramesh: hi suresh good morning"
+echo "suresh: hi ramesh good morning"
+echo "ramesh :how are you suresh"
+echo "suresh : iam good "
