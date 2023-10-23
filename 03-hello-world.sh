772ecc4672d64e8cf3565a6880dcd4f567854d11
srilakshmi <srilakshmidhulipalla34@gmail.com>
2023-10-23 12:42:42 -0400

    hellowrold script2

--- 03-hello-world.sh
+++ 03-hello-world.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo " hello shell i am learning devops with aws"
-PERSON1=Suresh
-PERSON2=Ramesh
+PERSON1=$1
+PERSON2=$2
 
 echo " $PERSON1: hi $PERSON2 how are you"
 echo "$PERSON2: i am good "
