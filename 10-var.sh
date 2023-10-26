409db8e6e5a819f8acc8b2dc8a27e953b0f7b8d7
srilakshmi <srilakshmidhulipalla34@gmail.com>
2023-10-26 15:42:58 -0400

    script

--- 10-var.sh
+++ 10-var.sh
@@ -6,7 +6,8 @@ empid=1413043
 echo srilakshmi empid $empid  
 companyname=tcs
 echo experinced in $companyname
-DATE=$(date)
+
+DATE=$(date -d "$days_ago days ago" '+%Y-%m-%d')    
  echo "tcs organazation leave: $DATE"
 
 
